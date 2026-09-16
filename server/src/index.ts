168cf16c2da15604f2e9a76e04b5ecf451a7748f
ShiwankAks <aksshiwank@gmail.com>
2026-09-16 16:33:15 +0530

    helmet added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
-
+import helmet from "helmet"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middlewares
+app.use(helmet())
 app.use(express.json())
 app.use(cors(
     {
